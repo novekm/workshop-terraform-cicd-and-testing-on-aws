065dedc226056634ca300242655ea8c74ad02ba5
Kevon Mayers <novekm@amazon.com>
2024-01-28 01:31:51 -0500

    update provider in devops folder

--- workshop-solution/aws-devops-core/provider.tf
+++ workshop-solution/aws-devops-core/provider.tf
@@ -7,12 +7,20 @@ terraform {
       version = "~> 5.0"
     }
   }
+  # backend "s3" {
+  #   bucket         = "mycomponents-tfstate"
+  #   key            = "state/terraform.tfstate"
+  #   region         = "eu-central-1"
+  #   encrypt        = true
+  #   dynamodb_table = "mycomponents_tf_lockid"
+  # }
 }
+
 
 # Configure the AWS Provider
 provider "aws" {
   region = var.aws_region
-  # TODO - ADD S3 BACKEND LATER
+
 
   default_tags {
     tags = {
