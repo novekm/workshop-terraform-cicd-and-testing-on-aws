fea9fd21ad13338035a5169e2f3a41e525063075
Kevon Mayers <novekm@amazon.com>
2024-01-28 01:33:16 -0500

    update provider.tf

--- workshop-solution/aws-devops-core/provider.tf
+++ workshop-solution/aws-devops-core/provider.tf
@@ -7,13 +7,14 @@ terraform {
       version = "~> 5.0"
     }
   }
-  # backend "s3" {
-  #   bucket         = "mycomponents-tfstate"
-  #   key            = "state/terraform.tfstate"
-  #   region         = "eu-central-1"
-  #   encrypt        = true
-  #   dynamodb_table = "mycomponents_tf_lockid"
-  # }
+  # Instructions: Add S3 Remote Backend Configuration
+  backend "s3" {
+    bucket         = "your-s3-bucket"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "your-tf-state-lock-dynamodb-table"
+  }
 }
 
 
