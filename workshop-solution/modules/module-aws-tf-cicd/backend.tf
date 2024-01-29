07a19088dcc37801b8e44ab75b6578378429ca86
Kevon Mayers <novekm@amazon.com>
2024-01-28 20:14:21 -0500

    update tf module in workshop solution folder

--- workshop-solution/modules/module-aws-tf-cicd/backend.tf
+++ workshop-solution/modules/module-aws-tf-cicd/backend.tf
@@ -1,12 +1,52 @@
-# Instructions: Create resources for S3 Remote Backend (Amazon S3 and DynamoDB)
+# Instructions: Dynamically create resources for S3 Remote Backend (Amazon S3 and DynamoDB)
 
-#  Terraform State S3 Bucket
-#  ADD S3 BUCKET FOR REMOTE STATE - USE CONDITIONAL VARIABLE
-# count = create_s3_remote_backend ? 1 : 0
+resource "random_string" "tf_remote_state_s3_buckets" {
+  for_each = var.tf_remote_state_resource_configs == null ? {} : var.tf_remote_state_resource_configs
+  length   = 4
+  special  = false
+  upper    = false
+}
+
+resource "aws_s3_bucket" "tf_remote_state_s3_buckets" {
+  for_each = var.tf_remote_state_resource_configs == null ? {} : var.tf_remote_state_resource_configs
+  bucket   = "${each.value.prefix}-tf-state-${random_string.tf_remote_state_s3_buckets[each.key].result}"
+
+}
+
+resource "aws_s3_bucket_versioning" "tf_remote_state_s3_buckets" {
+  for_each = var.tf_remote_state_resource_configs == null ? {} : var.tf_remote_state_resource_configs
+  bucket   = aws_s3_bucket.tf_remote_state_s3_buckets[each.key].id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "tf_remote_state_s3_buckets_pabs" {
+  for_each = var.tf_remote_state_resource_configs == null ? {} : var.tf_remote_state_resource_configs
+  bucket   = aws_s3_bucket.tf_remote_state_s3_buckets[each.key].id
+
+  block_public_acls       = var.s3_public_access_block
+  block_public_policy     = var.s3_public_access_block
+  ignore_public_acls      = var.s3_public_access_block
+  restrict_public_buckets = var.s3_public_access_block
+}
 
 # Terraform State Locking
-#  ADD DYNAMODB TABLE FOR REMOTE STATE LOCK - USE VARIABLE
-# count = create_s3_remote_backend ? 1 : 0
+resource "random_string" "tf_remote_state_lock_tables" {
+  for_each = var.tf_remote_state_resource_configs == null ? {} : var.tf_remote_state_resource_configs
+  length   = 4
+  special  = false
+  upper    = false
+}
+resource "aws_dynamodb_table" "tf_remote_state_lock_tables" {
+  for_each     = var.tf_remote_state_resource_configs == null ? {} : var.tf_remote_state_resource_configs
+  name         = "${each.value.prefix}-tf-state-lock-${random_string.tf_remote_state_lock_tables[each.key].result}"
+  billing_mode = each.value.ddb_billing_mode
+  hash_key     = each.value.ddb_hash_key
 
+  attribute {
+    name = each.value.ddb_hash_key
+    type = "S"
+  }
 
-# TODO - ADD S3 DYNAMODB LOCK
+}
--- workshop-solution/modules/module-aws-tf-cicd/variables.tf
+++ workshop-solution/modules/module-aws-tf-cicd/variables.tf
@@ -123,6 +123,29 @@ variable "s3_public_access_block" {
 
 }
 
+
+# Terraform Remote State Resources
+# - Codecommit -
+variable "tf_remote_state_resource_configs" {
+  type = map(object({
+    prefix           = optional(string, "my-prefix")
+    ddb_billing_mode = optional(string, "PAY_PER_REQUEST")
+    ddb_hash_key     = optional(string, "LockID")
+  }))
+  description = "Configurations for Terraform State Resources"
+  default     = {}
+
+  validation {
+    condition     = alltrue([for config in values(var.tf_remote_state_resource_configs) : length(config.prefix) > 3 && length(config.prefix) <= 40])
+    error_message = "The prefix of one of the defined Terraform Remote State Resource Configs is too long. A prefix can be a maxmium of 40 characters, as the names are used by other resources throughout this module. This can cause deployment failures for AWS resources with smaller character limits for naming. Please ensure all prefixes are 40 characters or less, and try again."
+  }
+
+  validation {
+    condition     = alltrue([for config in values(var.tf_remote_state_resource_configs) : config.ddb_billing_mode == "PAY_PER_REQUEST" || config.ddb_billing_mode == "PROVISIONED"])
+    error_message = "The DynamoDB Billing Mode ('ddb_billing_mode') of one of the defined Terraform Remote State Resource Configs is not an accepted value. Valid values for DynamoDB Billing Mode are 'PAY_PER_REQUEST' or 'PROVISIONED'. Please ensure the 'ddb_billing_mode' is set to one of these values and try again."
+  }
+}
+
 # - Project Prefix -
 variable "project_prefix" {
   type        = string
