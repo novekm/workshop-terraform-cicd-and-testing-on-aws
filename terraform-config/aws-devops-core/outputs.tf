7487c885de91315cda5c2ff5a6999cc23d078262
Kevon Mayers <novekm@amazon.com>
2024-01-28 20:10:00 -0500

    test aws-devops-core and copy from tf-config folder to ws-solution folder

---
+++ terraform-config/aws-devops-core/outputs.tf
@@ -0,0 +1,19 @@
+# AWS DevOps Core
+output "aws_devops_core_s3_bucket_name" {
+  value = module.module-aws-tf-cicd.tf_state_s3_buckets_names["aws_devops_core"]
+
+}
+output "aws_devops_core_ddb_table_name" {
+  value = module.module-aws-tf-cicd.tf_state_ddb_table_names["aws_devops_core"]
+
+}
+
+# Example Production Workload
+output "example_production_workload_s3_bucket_name" {
+  value = module.module-aws-tf-cicd.tf_state_s3_buckets_names["example_production_workload"]
+
+}
+output "example_production_workload_ddb_table_name" {
+  value = module.module-aws-tf-cicd.tf_state_ddb_table_names["example_production_workload"]
+
+}
